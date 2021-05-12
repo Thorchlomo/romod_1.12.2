4d501cffa3c4063aca1c34ca8a12b187bd3cbc47
Thorchlomo <henry.naitlho@gmail.com>
2021-05-12 10:13:01 +0200

    Javadoced basic_item.java

--- src/main/java/com/byethost33/wikimetns/item/basic_item.java
+++ src/main/java/com/byethost33/wikimetns/item/basic_item.java
@@ -2,9 +2,28 @@ package com.byethost33.wikimetns.item;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
-
+/**
+ * A class who is used to create generic-crafting item
+ * 
+ * 
+ * @since 1.2.0
+ * @version 1.0
+ * @author Thorchlomo
+ *
+ */
 public class basic_item extends Item{
-	
+	/**
+	 * <h2>Used to create generic item</h2>
+	 * Do not use for special items, it can only set name and creative tabs
+	 * 
+	 * 
+	 * @param [String] registryName
+	 * @param [String] unlocalizedName
+	 * @param [CreativeTabs] creativetab
+	 * 
+	 * @since 1.2.0
+	 * @author Thorchlomo
+	 */
 	public basic_item(String registryName, String unlocalizedName, CreativeTabs creativetab) {
 		this.setRegistryName(registryName);
 		this.setUnlocalizedName(unlocalizedName);

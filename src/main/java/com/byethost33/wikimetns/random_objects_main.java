d901eb52ba2da3e2234ad4831cd84629986869fe
Thorchlomo <henry.naitlho@gmail.com>
2021-07-17 22:53:54 +0200

    Todo : Rename RecipesRomodSMelter.java in RecipesRomodS**m**elter.java

--- src/main/java/com/byethost33/wikimetns/random_objects_main.java
+++ src/main/java/com/byethost33/wikimetns/random_objects_main.java
@@ -45,7 +45,7 @@ public class random_objects_main
     public static final String NAME = "random objects";
     public static final String VERSION = "1.2.0";
     
-    
+    //TODO rename RecipeRomodSMelter.java in RecipeRomodSmelter.java ;)
     
     @Mod.Instance(random_objects_main.MODID)
     public static random_objects_main instance;
